a9b057769883677ba35d21628f7027316618c471
Jan Grewe <jan.grewe@g-node.org>
2026-02-19 22:48:12 +0100

    [info] add an info.py file

---
+++ ampullary_ui/info.py
@@ -0,0 +1,9 @@
+import pathlib
+from packaging.version import Version
+
+organization_name = "de.bendalab"
+application_name = "AmpullaryUi"
+application_version = Version("0.0.1")
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent.parent
+DOCS_ROOT_FILE = pathlib.PosixPath.joinpath(PACKAGE_ROOT, "ampullary_ui", "docs", "index.md")
