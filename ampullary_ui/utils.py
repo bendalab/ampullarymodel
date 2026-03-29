da730b67b06f300c2439f8a009e7ed5906086e26
Jan Grewe <jan.grewe@g-node.org>
2026-03-29 12:22:24 +0200

    [utils] move to utils subpackage

--- ampullary_ui/utils.py
+++ ampullary_ui/utils.py
@@ -3,31 +3,6 @@ import numpy as np
 import ampullary_ui.resources_rc
 
 from brian2.units import usecond
-from PySide6.QtCore import QFile, QIODevice, QTextStream
 
 
-def load_json(resource):
-    f = QFile(resource)
-    if not f.open(QIODevice.ReadOnly | QIODevice.Text):
-        raise FileNotFoundError(resource)
-    common_variables = json.loads(bytes(f.readAll()).decode("utf-8"))
-    return common_variables
 
-
-def load_common_variables():
-    res = ":/configs/common_variables"
-    return load_json(res)
-
-
-def load_labels():
-    res = ":/configs/labels"
-    return load_json(res)
-
-
-def load_style():
-    style_file = QFile(":/configs/style")
-    style_file.open(QFile.ReadOnly | QFile.Text)
-    stream = QTextStream(style_file)
-    style = stream.readAll()
-    style_file.close()
-    return style
---
+++ ampullary_ui/utils/__init__.py
@@ -0,0 +1,29 @@
+import json
+
+from PySide6.QtCore import QFile, QIODevice, QTextStream
+
+def load_json(resource):
+    f = QFile(resource)
+    if not f.open(QIODevice.ReadOnly | QIODevice.Text):
+        raise FileNotFoundError(resource)
+    common_variables = json.loads(bytes(f.readAll()).decode("utf-8"))
+    return common_variables
+
+
+def load_common_variables():
+    res = ":/configs/common_variables"
+    return load_json(res)
+
+
+def load_labels():
+    res = ":/configs/labels"
+    return load_json(res)
+
+
+def load_style():
+    style_file = QFile(":/configs/style")
+    style_file.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(style_file)
+    style = stream.readAll()
+    style_file.close()
+    return style
