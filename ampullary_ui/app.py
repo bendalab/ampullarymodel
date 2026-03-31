c0a5b183411f322de95e2653e9813a5e8b5f7ef2
Jan Grewe <jan.grewe@g-node.org>
2026-03-31 11:19:34 +0200

    [app] add argparse and logging setup

--- ampullary_ui/app.py
+++ ampullary_ui/app.py
@@ -9,8 +9,32 @@ import ampullary_ui.resources_rc
 from ampullary_ui.utils import load_style
 from ampullary_ui.gui import MainWindow
 
+logging.basicConfig(level=logging.INFO, force=True)
+log_levels = {"critical": logging.CRITICAL, "error": logging.ERROR,
+              "warning":logging.WARNING, "info":logging.INFO,
+              "debug":logging.DEBUG}
 
-def main():
+
+def set_logging(loglevel):
+    logging.basicConfig(level=loglevel, force=True)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="AmpullaryUi. Tool for creating models of ampullary afferents")
+    parser.add_argument("-ll", "--loglevel", type=str, default="INFO",
+                        help=f"The log level that should be used. Valid levels are {[str(k) for k in log_levels.keys()]}")
+    return parser
+
+
+def main(args=None):
+    if args is None:
+        parser = create_parser()
+        args = parser.parse_args()
+        args.loglevel = log_levels[args.loglevel.lower() if args.loglevel.lower() in log_levels else "info"]
+
+    set_logging(args.loglevel)
+    logging.info("Starting Ampullary-GUI")
+
     app = QApplication()
     app.setApplicationName(info.application_name)
     app.setApplicationVersion(str(info.application_version))
@@ -43,5 +67,10 @@ def main():
     settings.setValue("app/pos_x", pos.x())
     settings.setValue("app/pos_y", pos.y())
 
+
 if __name__ == "__main__":
-    main()
+    parser = create_parser()
+    args = parser.parse_args()
+    args.loglevel = log_levels[args.loglevel.lower() if args.loglevel.lower() in log_levels else "info"]
+
+    main(args)
